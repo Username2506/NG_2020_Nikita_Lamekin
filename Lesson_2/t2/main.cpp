288a873fb670db5d455e69141a6b02b7781e92ec
Nikita Lamekin <jjpp1605@gmail.com>
2020-03-02 16:02:30 +0200

    Changed, so now there are comma between numbers

--- Lesson_2/t2/main.cpp
+++ Lesson_2/t2/main.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int main()
 {
     int a,i;
-    cout << "Vvedite kolichestvo zvezdochek: " << endl;
+    cout << "Vvedite chislo: " << endl;
     cin >> a;
     for (i=0; i<=a; i++)
     {
-        cout << i <<" ";
+        cout << i <<",";
     }
 }
 
