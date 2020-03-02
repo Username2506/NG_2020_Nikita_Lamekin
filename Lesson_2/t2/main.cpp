efbb6c2a3d01fe8687308285714591307d707f7b
Nikita Lamekin <jjpp1605@gmail.com>
2020-03-02 15:45:55 +0200

    Added Lesson_2 task2

---
+++ Lesson_2/t2/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,i;
+    cout << "Vvedite kolichestvo zvezdochek: " << endl;
+    cin >> a;
+    for (i=0; i<=a; i++)
+    {
+        cout << i <<" ";
+    }
+}
+
