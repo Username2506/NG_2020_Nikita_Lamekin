ee02ef754b16e9b890f63ea3abcb370b04b0563c
Nikita Lamekin <jjpp1605@gmail.com>
2020-03-08 19:47:17 +0200

    Added Lesson_2 task7

---
+++ Lesson_2/t7/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int chisla[20]; int counter; int biggest; int count_symbols;
+    for (counter = 0; counter < 20; counter++)
+        chisla[counter] = 0;
+    cout << "vvedite do 20 chisel: " << endl << "0 - zakonchit vvod" << endl;
+    for (counter = 0; counter < 20; counter++)
+    {
+        cin >> chisla[counter];
+        if (chisla [counter] == 0)
+            goto metka;
+    }
+    metka:
+    biggest = 0;
+    for (counter = 0; counter < 20; counter++)
+        if (chisla[counter] > biggest)
+            biggest = chisla[counter];
+    for (counter = 0; chisla[counter] != 0; counter++)
+    {
+        for (count_symbols = 0; count_symbols < (biggest - chisla[counter]) / 2; count_symbols++)
+            cout << " ";
+        for (count_symbols = 0; count_symbols < chisla[counter]; count_symbols++)
+            cout << "*";
+        cout << endl;
+    }
+}
