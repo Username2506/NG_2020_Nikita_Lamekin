0b1d656282b62bcf82b16460d3afbaf29df435fe
Nikita Lamekin <jjpp1605@gmail.com>
2020-03-20 16:30:47 +0200

    removed unwanted loop

--- Lesson_4/t3/main.cpp
+++ Lesson_4/t3/main.cpp
@@ -4,19 +4,14 @@ using namespace std;
 
 int main()
 {
-    char stroka[100], lowletters[] = "qwertyuiopasdfghjklzxcvbnm", bigletters[] = "QWERTYUIOPASDFGHJKLZXCVBNM";
-    int count_stroka = 0, count_letters;
+    char stroka[100];
+    int count_stroka = 0;
     cout << "Vvedite stroku: ";
     cin.getline(stroka,100);
     while (stroka[count_stroka] != 0)
     {
-        count_letters = 0;
-        while (lowletters[count_letters] != 0)
-        {
-            if (stroka[count_stroka] == lowletters[count_letters])
-                stroka[count_stroka] = bigletters[count_letters];
-            count_letters++;
-        }
+        if (stroka[count_stroka] > 96 and stroka[count_stroka] < 123)
+            stroka[count_stroka] -= 32;
         count_stroka++;
     }
     cout << stroka;
