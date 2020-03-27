d132cbed25acae23ea0db389fbcb784555a161f9
Nikita Lamekin <jjpp1605@gmail.com>
2020-03-27 14:51:57 +0200

    input to an array now in separate function

--- Lesson_5/t1/main.cpp
+++ Lesson_5/t1/main.cpp
@@ -2,19 +2,26 @@
 
 using namespace std;
 
-void umnozhenie_na_chislo(int array[],int razmer,int mnozhitel){
+void zapis_v_massiv(int array[], int razmer){
     int count;
     for (count = 0; count < razmer; count++)
     {
         cout << "Vvedite " << count+1 << "-oe chislo: ";
         cin >> array[count];
     }
+}
+
+void umnozhenie_na_chislo(int array[],int razmer,int mnozhitel){
+    int count;
     for (count = 0; count < razmer; count++)
         cout << count+1 << "-oe chislo posle umnozhenia: " << array[count]*mnozhitel << endl;
 }
 
 int main()
 {
-    int array[10];
-    umnozhenie_na_chislo(array,10,5);
+    int array[10], mnozhitel;
+    zapis_v_massiv(array,10);
+    cout << "Vvedite mnozhitel: ";
+    cin >> mnozhitel;
+    umnozhenie_na_chislo(array,10,mnozhitel);
 }
