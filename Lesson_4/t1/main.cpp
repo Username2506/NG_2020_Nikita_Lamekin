692cde75dbbc42d7406749d54d6dbbace9c74ea2
Nikita Lamekin <jjpp1605@gmail.com>
2020-03-27 14:39:12 +0200

    removed unnecessary condition in while

--- Lesson_4/t1/main.cpp
+++ Lesson_4/t1/main.cpp
@@ -7,7 +7,7 @@ int main()
     char stroka[100]; int slova = 0, counter = 0;
     cout << "Vvedite stroku: ";
     cin.getline(stroka,100);
-    while (stroka[counter] != 0 and counter <= 100)
+    while (stroka[counter] != 0)
     {
         if ((stroka[counter] >= 'A' and stroka[counter] <= 'Z') or (stroka[counter] >= 'a' and stroka[counter] <= 'z'))
             if (stroka[counter+1] < 'A' or (stroka[counter+1] > 'Z' and stroka[counter+1] < 'a') or stroka[counter+1] > 'z')
