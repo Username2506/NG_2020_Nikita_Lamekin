62ca9c3f665ab517f42e674d045d7576ec702404
Nikita Lamekin <jjpp1605@gmail.com>
2020-05-16 16:56:49 +0300

    Added database

---
+++ database/DB_Games/mainwindow.cpp
@@ -0,0 +1,95 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QSql>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    connect(ui->open_db, &QPushButton::clicked, this, &MainWindow::button_openPressed);
+    connect(ui->add, &QPushButton::clicked, this, &MainWindow::button_addPressed);
+    connect(ui->save, &QPushButton::clicked, this, &MainWindow::button_savePressed);
+    ui->table->setHorizontalHeaderLabels(QStringList() << "id" << "name" << "year" << "info");
+    ui->table->horizontalHeader()->resizeSection(3, 400);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::button_openPressed()
+{
+    if (ui->path->text() != "") {
+        db = QSqlDatabase::addDatabase("QSQLITE");
+        db.setDatabaseName(ui->path->text());
+        if (db.open() == false) {
+            ui->path->setText("Can't open database!");
+            return;
+        } else {
+            ui->path->setText("Connected to database!");
+        }
+        cmd = "SELECT * FROM games;";
+        QSqlQuery query;
+        query.exec(cmd);
+        int row = 0;
+        while (query.next()) {
+            row = query.value("id").toInt();
+        }
+        ui->table->setRowCount(row);
+        query.first();
+        ui->table->setItem(0, 0 , new QTableWidgetItem(query.value("id").toString()));
+        ui->table->setItem(0, 1 , new QTableWidgetItem(query.value("name").toString()));
+        ui->table->setItem(0, 2 , new QTableWidgetItem(query.value("year").toString()));
+        ui->table->setItem(0, 3 , new QTableWidgetItem(query.value("info").toString()));
+        row = 1;
+        while (query.next()) {
+            ui->table->setItem(row, 0 , new QTableWidgetItem(query.value("id").toString()));
+            ui->table->setItem(row, 1 , new QTableWidgetItem(query.value("name").toString()));
+            ui->table->setItem(row, 2 , new QTableWidgetItem(query.value("year").toString()));
+            ui->table->setItem(row, 3 , new QTableWidgetItem(query.value("info").toString()));
+            row++;
+        }
+        qDebug() << query.lastError();
+    }
+}
+
+void MainWindow::button_addPressed()
+{
+    ui->table->setRowCount(ui->table->rowCount() + 1);
+}
+
+void MainWindow::button_savePressed()
+{
+    if (db.isOpen()) {
+        cmd = "SELECT * FROM games;";
+        QSqlQuery query;
+        query.exec(cmd);
+        int row = 0;
+        while (query.next()) {
+            row = query.value("id").toInt();
+        }
+        query.first();
+        for (int count = 0; count < ui->table->rowCount(); count++) {
+            cmd = "UPDATE games SET name = '" + ui->table->item(count, 1)->text()
+                    + "' , year = '" + ui->table->item(count, 2)->text()
+                    + "' , info = '" + ui->table->item(count, 3)->text()
+                    + "' WHERE id = " + QString::number(count+1) + ";";
+            query.exec(cmd);
+            query.next();
+        }
+        for (int count = row; count < ui->table->rowCount(); count++) {
+            cmd = "INSERT INTO games (name, year, info) VALUES ('" + ui->table->item(count, 1)->text()
+                    + "', '" + ui->table->item(count, 2)->text()
+                    + "', '" + ui->table->item(count, 3)->text() + "');";
+            query.exec(cmd);
+        }
+    } else {
+        ui->path->setText("No database");
+    }
+}
+
---
+++ database/DB_Games/mainwindow.h
@@ -0,0 +1,32 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QSql>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QDebug>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void button_openPressed();
+    void button_addPressed();
+    void button_savePressed();
+
+private:
+    Ui::MainWindow *ui;
+    QSqlDatabase db;
+    QString cmd;
+};
+#endif // MAINWINDOW_H
